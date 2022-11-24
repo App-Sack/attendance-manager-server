e7b0c7e49cb9f8cd25ca63ea3745a2528b24fe80
chandansgowda <chandansuresh007@gmail.com>
2022-11-24 19:43:50 +0530

    added custom user model

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,46 @@
+"""
+Django admin customization.
+"""
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+from .models import User, Student, Course, Teacher, Semester, AttendanceRecord, OverallStudentAttendance, Section
 
-# Register your models here.
-from .models import Student, Course, Teacher, Semester, AttendanceRecord, OverallStudentAttendance, Section
+class UserAdmin(BaseUserAdmin):
+    """Define the admin pages for users."""
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal Info'), {'fields': ('name',)}),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
+    readonly_fields = ['last_login']
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            ),
+        }),
+    )
+
 
 class StudentAdmin(admin.ModelAdmin):
     list_display = ['usn', 'sr_no', 'name', 'sem', 'section']
@@ -21,6 +60,8 @@ class CourseAdmin(admin.ModelAdmin):
 class SectionAdmin(admin.ModelAdmin):
     list_display = ['section', 'sem']
 
+
+admin.site.register(User, UserAdmin)
 admin.site.register(Student, StudentAdmin)
 admin.site.register(Teacher, TeacherAdmin)
 admin.site.register(Course, CourseAdmin)
